153f92acc07bd4fa5c0a22bad8660a9f0fb24379
aksuz <zuzanna.burzynska@interia.pl>
2022-02-25 18:26:18 +0100

    fix project structure

--- src/main/java/com/politechnika/projekt/repository/ClientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.politechnika.projekt.repository;
-
-import com.politechnika.projekt.model.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client,Long> {
-
-    Client findByUsername(String username);
-}
